38d56dd5968a4b17fd2c18d74a2734181327dd8a
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-08-17 12:01:25 +0200

    added err handlings and some TODOs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import ReactWordcloud from 'react-wordcloud';
+import ReactWordcloud from "react-wordcloud";
+import { EmptyStatement } from "typescript";
 
 function WordCloud({ dataset }: { dataset: any }) {
-
   const options = {
     rotations: 2,
     rotationAngles: [0, 90],
     fontSizes: [20, 60],
-    fontWeight: 'bold',
+    fontWeight: "bold",
     padding: 3,
   };
 
@@ -18,7 +18,7 @@ function WordCloud({ dataset }: { dataset: any }) {
         //@ts-ignore
         <ReactWordcloud words={dataset} options={options} />
       ) : (
-        'No data to display'
+        "No data to display"
       )}
     </div>
   );
@@ -43,32 +43,39 @@ function RequirementModal({
 
 export function App() {
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<Error>();
+  const [error, setError] = useState<Error | null>(null);
   const [html, setHtml] = useState("");
   const [showModal, setShowModal] = useState(false);
   const [dataset, setDataset] = useState<any>(null);
   const [inputType, setInputType] = useState("rss");
   const [inputValue, setInputValue] = useState("");
 
-    const handleSubmit = async (e: { preventDefault: () => void; }) => {
+  const maxRetries = 3;
+
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     setIsLoading(true);
 
-    try {
-      const url = `/api/word-cloud/rss?url=${inputValue}`;
-      const response = await fetch(url);
+    for (let i = 0; i < maxRetries; i++) {
+      try {
+        const url = `/api/word-cloud/rss?url=${inputValue}`;
+        const response = await fetch(url);
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-        
-    const data = await response.json();
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
 
-    setDataset(data);
-    } catch (error) {
-      console.error('Failed to fetch word cloud data:', error);
-    } finally {
-      setIsLoading(false);
+        const data = await response.json();
+        setDataset(data);
+
+        break; // exit loop if successful
+      } catch (error) {
+        if (i === maxRetries - 1) {
+          setError(error as Error);
+        }
+      } finally {
+        setIsLoading(false);
+      }
     }
   };
 
@@ -80,27 +87,48 @@ export function App() {
       .finally(() => setIsLoading(false));
   }, []);
 
-  if (error != null) {
-    console.error(error);
-    return <div>Error! Check console...</div>;
+  if (error) {
+    return (
+      <div className="Error">
+        <h1>Failed to load challenge</h1>
+        <p>{error.message}</p>
+        //TODO: Add a retry button
+      </div>
+    );
   }
 
   return (
     <div className="App">
-            <button onClick={() => setShowModal(true)}>Show requirements</button>
+      <button onClick={() => setShowModal(true)}>Show requirements</button>
 
       <div className="controls">
-
         <div className="input-group">
           <div className="toggle">
-            <button onClick={() => setInputType("rss")} className={inputType === "rss" ? "active" : ""}>RSS</button>
+            <button
+              onClick={() => setInputType("rss")}
+              className={inputType === "rss" ? "active" : ""}
+            >
+              RSS
+            </button>
           </div>
-          <input type="text" value={inputValue} onChange={e => setInputValue(e.target.value)} placeholder="Enter valid rss URL" />
-          <button className="generateBtn" type="submit" onClick={handleSubmit}>Generate</button>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Enter valid rss URL"
+          />
+          <button className="generateBtn" type="submit" onClick={handleSubmit}>
+            Generate
+          </button>
         </div>
       </div>
 
-      {isLoading ? "Loading..." : null}
+      {isLoading ? (
+        <div className="loading">
+          <p>Loading ...</p>
+          //TODO: Add a spinner
+        </div>
+      ) : null}
 
       {showModal ? (
         <RequirementModal html={html} close={() => setShowModal(false)} />
